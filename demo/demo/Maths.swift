bd0353593474c7179fde98c9b597afbb8c5e7403
Maxime Britto <maxime@purplegiraffe.fr>
2021-04-29 15:23:24 +0200

    12_3_Coder_des_tests_unitaires_en_Swift v2

--- demo/demo/Maths.swift
+++ demo/demo/Maths.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 func calculPuissance(nombre:Double, exposant:Double) -> Double {
-    return pow(nombre, exposant)
+    if exposant == 0 {
+        return 1
+    } else if exposant < 0 {
+        return 1 / calculPuissance(nombre: nombre, exposant: -exposant)
+    } else {
+        // 4p2 = 4 * (4 * 1)
+        return nombre * calculPuissance(nombre: nombre, exposant: exposant - 1)
+    }
 }
--- demo/demoTests/demoTests.swift
+++ demo/demoTests/demoTests.swift
@@ -21,7 +21,10 @@ class demoTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+        XCTAssertEqual(calculPuissance(nombre: 4, exposant: 2), 16)
+        XCTAssertEqual(calculPuissance(nombre: 3, exposant: 4), 81)
+        XCTAssertEqual(calculPuissance(nombre: 3, exposant: 0), 1)
+        XCTAssertEqual(calculPuissance(nombre: 3, exposant: -1), 0.3333, accuracy: 0.0001)
     }
 
     func testPerformanceExample() throws {
